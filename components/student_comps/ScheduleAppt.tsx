6bac8bf70b4d496fb742f992bc7b6105145b0b0c
galvanitic <develop@rglvn.com>
2021-12-30 11:07:16 -0400

    Added some styling for scheduling component. Thinking of changing the UI a bit more.

--- components/student_comps/ScheduleAppt.tsx
+++ components/student_comps/ScheduleAppt.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 // import Image from 'next/image';
 import Avatar from '@mui/material/Avatar';
+import CoachIcon from '@mui/icons-material/Sports';
 interface IScheduleApptProps {
 }
 
@@ -8,10 +9,15 @@ const ScheduleAppt: React.FunctionComponent<IScheduleApptProps> = (props) => {
   return (
     <div className="schedule_appt_cont widget_cont">
       <div className="schedule_appt widget">
-        <h2>Schedule a Meeting with Your Coach</h2>
+        <div className="schedule_text">
+          <CoachIcon />
+          <h2>Schedule a Meeting with Your Coach</h2>
+        </div>
         <div className="coach_profile">
-          <Avatar alt="Cynthia Suarez" src=""/>
-          <div></div>
+          <Avatar sx={{marginRight: '10px'}} alt="Cynthia Suarez" src=""/>
+          <div>
+            <h3>Cynthia Suarez</h3>
+          </div>
         </div>
       </div>
     </div>
